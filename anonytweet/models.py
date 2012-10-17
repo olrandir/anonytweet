f11506ce0ce2a38afc8eb584e91900f6c901dc8f
Dimitris John Raptis <olrandir@gmail.com>
2012-10-17 23:17:47 +0300

    added initial Tweeter class, with hashing for ip saving

---
+++ anonytweet/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+import django.dispatch
+from django.contrib.auth.hashers import check_password, make_password
+
+"""Links an IP to a 2-character 'username' """
+class Tweeter(models.Model):
+    username = models.CharField(max_length=2)
+    ip = models.CharField(max_length=128, unique=True)
+
+    class Meta:
+        db_table = 'tweeters'
+        verbose_name = 'Tweeter'
+        verbose_name_plural = 'Tweeters'
+    
+    def __unicode__(self):
+        return "%s [%s]" % (self.username, self.ip)
+
+    def save(self, *args, **kwargs):
+	    self.ip = make_password(self.ip, salt="0")
+	    super(Tweeter, self).save(*args, **kwargs)
