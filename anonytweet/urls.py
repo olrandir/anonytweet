a21abf73e952d6d6203684c927abf4ab1ae409cb
Dimitris John Raptis <olrandir@gmail.com>
2012-10-17 23:16:46 +0300

    inital urls.py

--- anonytweet/urls.py
+++ anonytweet/urls.py
@@ -6,12 +6,12 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'anonytweet.views.home', name='home'),
+    url(r'^$', 'anonytweet.views.home', name='home'),
     # url(r'^anonytweet/', include('anonytweet.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
